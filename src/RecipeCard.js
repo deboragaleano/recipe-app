4db66df53a78394834a26f5f6668440c5a1f6fc0
Deb <dgaleanoservian@gmail.com>
2020-05-10 16:49:11 +0200

    fix list items

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import { 
-    Card, CardImg, CardText, CardBody, 
-    CardTitle, Badge, Button 
-} from 'reactstrap';
+    Card, CardImg, CardTitle, ListGroup, 
+    Button, ListGroupItem } from 'reactstrap';
 
 class RecipeCard extends Component {
     render() {
         return(
             <Card body>
                 <div className="d-flex mb-2 justify-content-between">
-                    <CardTitle className="mb-0 font-weight-bold">{this.props.data.name}</CardTitle>
+                    <CardTitle className="mb-0 font-weight-bold text-center">{this.props.data.name}</CardTitle>
                 </div>
                 <CardImg top width="100%" src={this.props.data.image} alt={this.props.data.name}/> 
-                <CardText className="text-left">
-                    <strong>Ingredients:</strong>{this.props.data.ingredients.split('\n').map((i, key) => (
-                    <li key={key}>{i}</li>))}
-                </CardText>
+                <div className="card-text text-left">
+                    <strong>Ingredients:</strong>
+                    <ListGroup >
+                        {this.props.data.ingredients.split('\n').map((i, key) => (
+                        <ListGroupItem className="hidden" key={key}>{i}</ListGroupItem>))}
+                    </ListGroup>
+                </div>
                 <Button 
                     href={this.props.data.url}
                     target="_blank"
