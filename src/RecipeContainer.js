eb083cf3c4e1eff7e02c08a2ba5ce0c95e7eccc9
Deb <dgaleanoservian@gmail.com>
2020-05-10 15:39:11 +0200

    toggle title

--- src/RecipeContainer.js
+++ src/RecipeContainer.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import data from './data/data.json'; 
 import RecipeCard from "./RecipeCard"; 
 import {random} from './helpers';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+
 import { 
     Container, Row, Col, Button, 
     Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
@@ -17,7 +18,8 @@ class RecipeContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            recipe: null,
+            recipe: null,   
+            isClicked: false
         }
         this.handleClick = this.handleClick.bind(this); 
     } 
@@ -26,7 +28,8 @@ class RecipeContainer extends Component {
         let randomRecipes = Array.from({length: this.props.numRecipes}, () => random(this.props.recipes));
         let UniqueRecipes = [...new Set(randomRecipes)]
         this.setState({
-            recipe: UniqueRecipes
+            recipe: UniqueRecipes,
+            isClicked: true
         })
     }
 
@@ -40,13 +43,12 @@ class RecipeContainer extends Component {
                 <Navbar color="dark" dark expand="md">
                     <NavbarBrand href="#">Recipe App</NavbarBrand>
                 </Navbar>
+                
                 <Jumbotron>
-                {/*TODO: Toggle the titles when clicking the button*/}
-                <h1 className="display-3">Make a new recipe today!</h1>
-                <p className="lead">Learn how to cook something new. Check a random choice below! 
-                {/* <FontAwesomeIcon icon={faThumbsUp}/> */}
-                </p>
-                    <p>
+                    {!this.state.isClicked && <div>
+                    <h1 className="display-3">Make a new recipe today!</h1>
+                    <p className="lead">Learn how to cook something new. Check a random choice below! <FontAwesomeIcon icon={faThumbsUp}/></p></div>}
+                    {this.state.isClicked && <h1>Pick one and start cookin'!</h1>}
                     <Row>
                         {this.state.recipe && this.state.recipe.map(r => ( 
                             <Col xs="4" key={`${r.name}`}>
@@ -54,13 +56,14 @@ class RecipeContainer extends Component {
                             </Col>
                         ))}
                     </Row>
+
                     <Button
                         color='primary'
                         size='lg'
                         onClick={this.handleClick}>
                         Click me!
                     </Button>
-                    </p>
+
                 </Jumbotron>
             </Container>
             )
